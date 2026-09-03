8ff161db297e0b04f56f1fb18828675d9dfaf9b2
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2026-09-03 14:28:47 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/prolog-cycling/',
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+});
